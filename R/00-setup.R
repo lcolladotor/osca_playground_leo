3b58fcbc76e8c6b70807b8f0e75931ab32386485
Leonardo Collado Torres <lcolladotor@gmail.com>
2020-03-22 23:39:37 -0400

    start repo

---
+++ R/00-setup.R
@@ -0,0 +1,9 @@
+## Create project
+usethis::create_project('~/Desktop/osca_playground_leo')
+
+## Start this setup file
+usethis::use_r('00-setup.R')
+
+## Start GitHub repo
+usethis::use_git()
+usethis::use_github()
