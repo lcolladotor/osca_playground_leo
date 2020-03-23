2699374c7a2f8fdce0e553b6d9434416f97320d2
Leonardo Collado Torres <lcolladotor@gmail.com>
2020-03-23 10:28:21 -0400

    Intro notes

--- R/00-setup.R
+++ R/00-setup.R
@@ -10,4 +10,7 @@ usethis::use_git()
 ## Use GitHub
 usethis::browse_github_token()
 usethis::edit_r_environ() ## then restart R
-usethis::use_github()
+usethis::use_github() ## commit first, then run this command
+
+## Start 01-intro notes
+usethis::use_r('01-introduction.R')
